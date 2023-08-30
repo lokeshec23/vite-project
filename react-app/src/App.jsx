5ab58efbb12076023f7447aa2cb43244a5a56528
lokeshec23 <lokeshs.ece2019@dce.edu.in>
2023-08-30 14:19:01 +0530

    update file

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,10 +1,82 @@
-function App(){
+import { useState } from "react";
+import "./App.css";
+function App() {
+  let [todoInput, updateInput] = useState("");
+  let [todoList, updateTodos] = useState([
+    {
+      id: 1,
+      task: "Learn React",
+    },
+    {
+      id: 2,
+      task: "Learn PC",
+    },
+  ]);
+  let nextId = 3;
+  function addNewTodo() {
+    if (todoInput == "") {
+      alert("Add some task");
+    } else {
+      let newTodos = [
+        ...todoList,
+        {
+          id: nextId++,
+          task: todoInput,
+        },
+      ];
+      updateTodos(newTodos);
+      updateInput("");
+    }
+  }
 
-    return (
-        <>
-            
-        </>
-    )
+  function deleteTodo(id) {
+    let updatedTodos = todoList.filter((todo) => {
+      return todo.id != id;
+    });
+    updateTodos(updatedTodos);
+  }
+  return (
+    <div className="container mt-5 w-50">
+      <h3 className="text-cente m">ToDo List App Using React </h3>
+      <div className="input-group">
+        <input
+          onChange={(e) => {
+            let task = e.target.value;
+            updateInput(task);
+          }}
+          type="text"
+          value={todoInput}
+          className="form-control"
+          placeholder="Enter Item Here"
+        />
+        <button
+          className="btn btn-primary"
+          onClick={() => {
+            addNewTodo();
+          }}
+        >
+          Add
+        </button>
+      </div>
+      <ul className="list-group mt-4">
+        {todoList.map((todo) => {
+          return (
+            <li key={todo.id} className="list-group-item">
+              <p>{todo.task}</p>
+              <button
+                className="btn"
+                onClick={() => {
+                  deleteTodo(todo.id);
+                }}
+              >
+                ❌
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
